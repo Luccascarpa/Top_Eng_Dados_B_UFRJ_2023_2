2cc2be40cf0803fafbb5203fe5f695db5aa1c5e1
Luccascarpa <ms-lucca1@hotmail.com>
2023-11-07 12:02:08 -0300

    update queue file

--- Ex 3 - Hierarquia de Estruturas de Dados/ex_3_queue.py
+++ Ex 3 - Hierarquia de Estruturas de Dados/ex_3_queue.py
@@ -1,77 +1,77 @@
 import datetime
 
 class Node:
-    def __init__(self, user, tempo_atendimento):
+    def __init__(self, user, service_time):
         self.user = user
-        self.tempo_espera = 0
-        self.tempo_atendimento = tempo_atendimento
-        self.hora_para_retirada = None
+        self.wait_time = 0
+        self.service_time = service_time
+        self.pickup_time = None
         self.next = None
         
 
-class FilaSimples:
+class Queue:
     def __init__(self):
-        self.front = None  # início da fila
-        self.rear = None  # fim da fila
+        self.front = None  # início da queue
+        self.back = None  # fim da queue
         self.size = 0
 
-    def esta_vazia(self):
+    def is_empty(self):
         return self.front is None
 
-    def entrar_na_fila(self, user, tempo_atendimento):
-        new_node = Node(user, tempo_atendimento)
+    def push(self, user, service_time): # entrar na queue
+        new_node = Node(user, service_time)
         
-        if self.rear is None:
-            self.front = self.rear = new_node
+        if self.back is None:
+            self.front = self.back = new_node
         else:
-            self.rear.next = new_node
-            self.rear = new_node
+            self.back.next = new_node
+            self.back = new_node
 
         self.size += 1
 
-    def sair_da_fila(self):
+    def pop(self): # sair da queue
         if self.esta_vazia():
             return None
 
         user = self.front.user
         self.front = self.front.next
         if self.front is None:
-            self.rear = None
+            self.back = None
         self.size -= 1
         return user
 
-    def atualizar_tempos_de_espera(self, tempo_real_horario_entrega):
+    def update_wait_times(self, real_delivery_time):
         current = self.front
-        tempo_real = datetime.datetime.strptime(tempo_real_horario_entrega, '%H:%M')
-        posicao = 1  # Variável para controlar a posição na fila
+        real_time = datetime.datetime.strptime(real_delivery_time, '%H:%M')
+        position = 1  # Variável para controlar a posição na queue
 
         while current is not None:
-            # Cálculo do tempo estimado de atendimento baseado na posição na fila e no tempo médio de atendimento
-            current.tempo_espera = posicao * current.tempo_atendimento
+            # Cálculo do tempo estimado de atendimento baseado na posição na queue e no tempo médio de atendimento
+            current.wait_time = position * current.service_time
 
             # Contador regressivo de tempo para atendimento
-            tempo_entrega = tempo_real + datetime.timedelta(minutes=current.tempo_espera)
-            current.hora_para_retirada = tempo_entrega.strftime('%H:%M')
+            tempo_entrega = real_time + datetime.timedelta(minutes=current.wait_time)
+            current.pickup_time = tempo_entrega.strftime('%H:%M')
             
-            # Incremento da posição para considerar o próximo usuário na fila
-            posicao += 1
+            # Incremento da posição para considerar o próximo usuário na queue
+            position += 1
 
             current = current.next
 
-# Exemplo de uso da FilaSimples (Problema do Bandejão)
-fila = FilaSimples()
+# Exemplo de uso da Queue (Problema do Bandejão)
+queue = Queue()
 
-# Adicionando usuários na fila (Usuário, tempo de atendimento em minutos)
-fila.entrar_na_fila("Usuário 1", 5)
-fila.entrar_na_fila("Usuário 2", 5)
-fila.entrar_na_fila("Usuário 3", 5)
+# Adicionando usuários na queue (Usuário, tempo de atendimento em minutos)
+queue.push("Usuário 1", 5)
+queue.push("Usuário 2", 5)
+queue.push("Usuário 3", 5)
 
 # Simulação de retirada dos pratos
-fila.atualizar_tempos_de_espera('12:15')  # Simula o tempo real de entrega do prato
+queue.update_wait_times('12:15')  # Simula o tempo real de entrega do prato
 
-# Verificando a fila após atualização dos tempos de espera
-print("Fila após atualização dos tempos de espera:")
-current = fila.front
+# Verificando a queue após atualização dos tempos de espera
+print("fila após atualização dos tempos de espera:")
+current = queue.front
 while current is not None:
-    print(f"Usuário: {current.user}, Tempo para retirada: {current.hora_para_retirada}")
+    print(f"Usuário: {current.user}, Tempo para retirada: {current.pickup_time}")
     current = current.next
